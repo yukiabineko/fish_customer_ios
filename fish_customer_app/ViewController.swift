749991457dbf14864057c25ab243564ab9e18965
msk-ya <masakiabineko@gmail.com>
2021-02-06 15:45:53 +0900

    Initial Commit

---
+++ fish_customer_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fish_customer_app
+//
+//  Created by abi on 2021/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ fish_customer_appTests/fish_customer_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  fish_customer_appTests.swift
+//  fish_customer_appTests
+//
+//  Created by abi on 2021/02/06.
+//
+
+import XCTest
+@testable import fish_customer_app
+
+class fish_customer_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
