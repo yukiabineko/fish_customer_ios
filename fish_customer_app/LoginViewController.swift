efef144344ee4fec8e14d880897cecab113580ee
msk-ya <masakiabineko@gmail.com>
2021-02-08 00:20:02 +0900

    簡易レイアウト

--- fish_customer_app/LoginViewController.swift
+++ fish_customer_app/LoginViewController.swift
@@ -1,25 +1,22 @@
 //
-//  NewUsersViewController.swift
+//  LoginViewController.swift
 //  fish_customer_app
 //
-//  Created by 植松勇貴 on 2021/02/07.
+//  Created by abi on 2021/02/07.
 //
 
 import UIKit
 
-class NewUsersViewController: UIViewController, UITextFieldDelegate {
-    @IBOutlet weak var name_field: UITextField!
+class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var mail_field: UITextField!
     @IBOutlet weak var pass_field: UITextField!
-    @IBOutlet weak var pass_conf_field: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        name_field.delegate = self
+        
         mail_field.delegate = self
         pass_field.delegate = self
-        pass_conf_field.delegate = self
-       
+        
     }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
