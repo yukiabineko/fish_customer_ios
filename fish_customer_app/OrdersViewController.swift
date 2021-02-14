ab5386bb4ce1f117e2993556bd6de075e4122be8
msk-ya <masakiabineko@gmail.com>
2021-02-14 21:06:10 +0900

    お客様情報

--- fish_customer_app/OrdersViewController.swift
+++ fish_customer_app/OrdersViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class OrdersViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     let sessionTitle = ["本日受け取り商品", "明日の予約商品"]
-    let todayOrder = MyData().todayOrders().today
-    let tomorrowOrder = MyData().todayOrders().tomorrow
+    var todayOrder:Array<Any> = []
+    var tomorrowOrder:Array<Any> = []
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        todayOrder = MyData().todayOrders().today
+        tomorrowOrder = MyData().todayOrders().tomorrow
 
     }
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -35,32 +38,38 @@ class OrdersViewController: UIViewController,UITableViewDelegate,UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "orderCell", for: indexPath) as! OrderTableViewCell
-        let todayRow = todayOrder[indexPath.row] as! Dictionary<String, Any>
-        /*let tommorowRow = tomorrowOrder[indexPath.row] as! Dictionary<String, Any>*/
+       
+       
         switch indexPath.section {
         case 0:
-            cell.setOrderItem(
-                name: todayRow["name"] as! String,
-                price: String(describing: todayRow["price"]!),
-                num: String(describing: todayRow["num"]!),
-                process: todayRow["process"] as! String,
-                status:String(describing: todayRow["status"]!)
-                
-            )
-        /*case 1:
-            cell.setOrderItem(
-                name: tommorowRow["name"] as! String,
-                price: tommorowRow["price"] as! String,
-                num: tommorowRow["num"] as! String,
-                process: tommorowRow["process"] as! String,
-                status: tommorowRow["status"] as! String
-                
-            )
- */
+            if(todayOrder.count > 0){
+                let todayRow = todayOrder[indexPath.row] as! Dictionary<String, Any>
+                cell.setOrderItem(
+                    name: todayRow["name"] as! String,
+                    price: String(describing: todayRow["price"]!),
+                    num: String(describing: todayRow["num"]!),
+                    process: todayRow["process"] as! String,
+                    status:String(describing: todayRow["status"]!)
+                )
+            }
+        case 1:
+            if(tomorrowOrder.count > 0){
+                let tommorowRow = tomorrowOrder[indexPath.row] as! Dictionary<String, Any>
+                cell.setOrderItem(
+                    name: tommorowRow["name"] as! String,
+                    price: String(describing: tommorowRow["price"]!),
+                    num: String(describing: tommorowRow["num"]!),
+                    process: tommorowRow["process"] as! String,
+                    status:String(describing: tommorowRow["status"]!)
+                )
+            }
         default:
             break
         }
       return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
     }
     
 }
