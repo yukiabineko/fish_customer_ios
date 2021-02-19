46a5b57951d78c79f0b928b17cc8f64583985480
abineko2 <yuki1980426@gmail.com>
2021-02-20 07:43:23 +0900

    2/20　プログレスバー

--- fish_customer_app/ViewController.swift
+++ fish_customer_app/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController{
     @IBOutlet weak var order_button: UIButton!
     @IBOutlet weak var customer_button: UIButton!
     @IBOutlet weak var login_tag: UIButton!
+    var progressArea:UIView!
+    var progressLabel:UILabel!
+    var progressBar:UIProgressView!
+    
+    
     /*データモデル*/
     
     struct Items: Codable{
@@ -27,6 +32,23 @@ class ViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        progressArea = UIView.init(frame: CGRect(x: 0, y: self.view.bounds.size.height/3, width: self.view.bounds.size.width, height: self.view.bounds.size.height/10 ))
+        progressArea.backgroundColor = UIColor.white
+        
+        progressLabel = UILabel(frame: CGRect(x: 0, y: progressArea.frame.size.height/2.5, width: progressArea.frame.size.width, height: progressArea.frame.size.height/15))
+        progressLabel.text = "しばらくお待ちください。"
+        
+        progressBar = UIProgressView(frame: CGRect(x: 10, y: progressArea.frame.size.height/2, width: progressArea.frame.size.width-20, height: progressArea.frame.size.height/20))
+        progressBar.transform = CGAffineTransform(scaleX: 1.0, y: 6.0)
+        progressBar.progressTintColor = .blue
+        progressBar.progress = 1
+
+        
+        progressArea.addSubview(progressLabel)
+        progressArea.addSubview(progressBar)
+        self.view.addSubview(progressArea)
+        
         
         if(item_data.count == 0){
             let url = URL(string: "https://uematsu-backend.herokuapp.com/orders")!
@@ -87,10 +109,11 @@ class ViewController: UIViewController{
         else{
             let alert:UIAlertController = UIAlertController(title: "ログアウト", message: "ログアウトしますよろしいでしょうか？", preferredStyle: .alert)
             let action = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
-            let logoutAction = UIAlertAction(title: "ログアウト", style: .default, handler: {(action: UIAlertAction) -> Void in
+            let logoutAction = UIAlertAction(title: "ログアウト", style: .default, handler: { [self](action: UIAlertAction) -> Void in
                 user_data.removeAll()
                 self.loadView()
                 self.viewDidLoad()
+                self.login_tag.setTitle("ログイン", for: .normal)
                 let checkAlert = UIAlertController(title: "確認", message: "ログアウトしました。", preferredStyle: .alert)
                 let closeAction = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
                 checkAlert.addAction(closeAction)
